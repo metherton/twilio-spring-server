d3ae92693dc131b4a22980b5608dccb505f7c827
metherton <martinetherton63@googlemail.com>
2015-11-22 20:07:19 +0100

    first commit

---
+++ src/main/java/com/martinetherton/twilio/server/TwilioSpringServerApplication.java
@@ -0,0 +1,36 @@
+package com.martinetherton.twilio.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@SpringBootApplication
+@RestController
+@EnableRedisHttpSession
+public class TwilioSpringServerApplication {
+
+    @RequestMapping("/")
+    public Map<String,Object> home() {
+        Map<String,Object> model = new HashMap<String,Object>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World");
+        return model;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwilioSpringServerApplication.class, args);
+    }
+
+    @Bean
+    HeaderHttpSessionStrategy sessionStrategy() {
+        return new HeaderHttpSessionStrategy();
+    }
+}
