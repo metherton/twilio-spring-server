9c5f9330b4a8ab8f06778eb827d582b7fa9a3765
metherton <martinetherton63@gmail.com>
2016-01-07 14:06:04 +0100

    working with security

--- src/main/java/com/martinetherton/twilio/server/TwilioSpringServerApplication.java
+++ src/main/java/com/martinetherton/twilio/server/TwilioSpringServerApplication.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 //import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 //import org.springframework.session.web.http.HeaderHttpSessionStrategy;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -23,49 +24,13 @@ import java.util.UUID;
 
 @SpringBootApplication
 @RestController
-//@EnableRedisHttpSession
-public class TwilioSpringServerApplication {
-//public class TwilioSpringServerApplication extends WebMvcConfigurerAdapter {
+@EnableRedisHttpSession
+public class TwilioSpringServerApplication extends WebMvcConfigurerAdapter {
 
-//    @Override
-//    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-//        configurer.defaultContentType(MediaType.APPLICATION_XML);
-//    }
-
-//    @RequestMapping("/")
-//    public Map<String,Object> home() {
-//        Map<String,Object> model = new HashMap<String,Object>();
-//        model.put("id", UUID.randomUUID().toString());
-//        model.put("content", "Hello World");
-//        return model;
-//    }
-//
-//    @RequestMapping(path="/incoming_call", method=RequestMethod.GET)
-//    public @ResponseBody TwiMLResponse routeCall(HttpServletRequest request, HttpServletResponse response) {
-//
-//        TwiMLResponse twiml = new TwiMLResponse();
-//        final Gather gather = new Gather();
-//        gather.setNumDigits(1);
-//        gather.setTimeout(5);
-//        gather.setAction("enqueue_call");
-//
-//        final Say sayEspanol = new Say("Para Espanol oprime el uno.");
-//        sayEspanol.setLanguage("es");
-//
-//        final Say sayEnglish = new Say("For English, please hold or press two.");
-//        sayEnglish.setLanguage("en");
-//
-//        try {
-//            gather.append(sayEspanol);
-//            gather.append(sayEnglish);
-//            twiml.append(gather);
-//        } catch (final TwiMLException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-//
-//        return twiml;
-//    }
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_XML);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TwilioSpringServerApplication.class, args);
--- src/main/java/com/martinetherton/twilio/server/WebSecurityConfig.java
+++ src/main/java/com/martinetherton/twilio/server/WebSecurityConfig.java
@@ -1,29 +1,42 @@
-//package com.martinetherton.twilio.server;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
-//
-///**
-// * Created by martin on 06/12/15.
-// */
-//@Configuration
-//@EnableWebMvcSecurity
-//public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests().antMatchers("/", "/incoming_call","/incoming_call/enqueue_call","/incoming_call/**", "/enqueue_call").permitAll().anyRequest().authenticated();
+package com.martinetherton.twilio.server;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+/**
+ * Created by martin on 06/12/15.
+ */
+@Configuration
+@EnableWebMvcSecurity
+public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity webSecurity) throws Exception
+    {
+        webSecurity
+                .ignoring()
+                // All of Spring Security will ignore the requests
+                .antMatchers(HttpMethod.POST, "/enqueue_call");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/incoming_call").permitAll()
+                .anyRequest().authenticated();
+    }
+
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//                .inMemoryAuthentication()
+//                .withUser("user").password("password").roles("USER");
 //    }
-//
-////    @Autowired
-////    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-////        auth
-////                .inMemoryAuthentication()
-////                .withUser("user").password("password").roles("USER");
-////    }
-//
-//}
+
+}
