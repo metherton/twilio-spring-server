6818d79dfd016191146c74cd7e3879db477074d1
metherton <martinetherton63@gmail.com>
2016-01-07 19:21:13 +0100

    automatically accept reservation

--- src/main/java/com/martinetherton/twilio/server/TaskRouterController.java
+++ src/main/java/com/martinetherton/twilio/server/TaskRouterController.java
@@ -34,9 +34,9 @@ public class TaskRouterController {
         return twimlResponseBuilder.enqueueCall();
     }
 
-    @RequestMapping(path="assignments",method=RequestMethod.POST)
+    @RequestMapping(path="assignments.json",method=RequestMethod.POST)
     public @ResponseBody String assignments() throws IOException, TwiMLException {
-        return "{}";
+        return "{\"instruction\":\"accept\"}";
     }
 
 }
